97c7316de301417baf41af63c271e795c2c80083
Maxim Cousineau <macouap@l4714-11.info.polymtl.ca>
2017-09-27 17:30:52 -0400

    MC affichage de la map

--- velomtl.js
+++ velomtl.js
@@ -1,4 +1,15 @@
-map = new google.maps.Map(document.getElementById('map'), {
-  center: {lat: -34.397, lng: 150.644},
-  zoom: 8
-});
+
+
+function initMap() {    
+    var bixi = $.ajax('https://secure.bixi.com/data/stations.json', {dataType: "json"});
+    
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 45.55, lng:-73.7 },
+        zoom: 10
+    });
+    
+    var mark = new google.maps.Marker({
+        position: {lat: bixi.stations[0].la, lng: bixi.stations[0].lo},
+        map: map
+    });
+}
