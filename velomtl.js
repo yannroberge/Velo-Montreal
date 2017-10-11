abe95d38de351ae8c59cb19ceb5cf11dec5330d3
Max <maxac595@hotmail.com>
2017-10-11 19:42:01 -0400

    M.C. Onglets et autocomplétion

--- velomtl.js
+++ velomtl.js
@@ -1,23 +1,60 @@
 
 function initMap() {    
-    var map = new google.maps.Map(document.getElementById('map'), {
+    var carte = new google.maps.Map(document.getElementById('div-map'), {
         center: {lat: 45.55, lng:-73.7},
-        zoom: 10
+        zoom: 10,
+        minZoom: 10
     });
-    
+}
+
+$( function() {
     
     $.ajax('https://secure.bixi.com/data/stations.json',
         {
             success: function(data){
-                var bixi = data
-                var mark = new google.maps.Marker({
-                    position: {lat: data.stations[0].la, lng: data.stations[0].lo},
-                    map: map
-                })
+                var bixi = data;
+                var nomsStations = $.map(bixi.stations, function(station){ return station.s; });
+                initAutocomplete(nomsStations);
             },
-            error: function(jqXHR, status, error){
-                alert(error);
+            error: function(){
+                alert("Erreur lors du chargement des stations");
             }
         }
     );
+    
+    
+});
+
+function initAutocomplete(nomsStations){
+    //Code tiré du tutoriel sur le widget Autocomplete de jQuery UI
+    //http://jqueryui.com/autocomplete/#folding
+    
+    var accentMap = {
+        //Cette partie est modifiée pour accomoder les lettres accentuées
+        //présentes dans les noms de stations
+        "à": "a",
+        "â": "a",
+        "è": "e",
+        "é": "e",
+        "ê": "e",
+        "ô": "o"
+    };
+    
+    var normalize = function( term ) {
+        var ret = "";
+        for ( var i = 0; i < term.length; i++ ) {
+          ret += accentMap[ term.charAt(i) ] || term.charAt(i);
+        }
+        return ret;
+    };
+    
+    $( "#recherche" ).autocomplete({
+        source: function( request, response ) {
+        var matcher = new RegExp( $.ui.autocomplete.escapeRegex( request.term ), "i" );
+        response( $.grep( nomsStations, function( value ) {
+          value = value.label || value.value || value;
+          return matcher.test( value ) || matcher.test( normalize( value ) );
+        }) );
+      }
+    });
 }
