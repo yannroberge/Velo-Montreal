b8aa86d7cdf9877be02e672743e79b2d505a709b
Max <maxac595@hotmail.com>
2017-10-11 14:18:29 -0400

    M.C. Requête AJAX fonctionnelle

--- velomtl.js
+++ velomtl.js
@@ -1,15 +1,23 @@
 
-
 function initMap() {    
-    var bixi = $.ajax('https://secure.bixi.com/data/stations.json', {dataType: "json"});
-    
     var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 45.55, lng:-73.7 },
+        center: {lat: 45.55, lng:-73.7},
         zoom: 10
     });
     
-    var mark = new google.maps.Marker({
-        position: {lat: bixi.stations[0].la, lng: bixi.stations[0].lo},
-        map: map
-    });
+    
+    $.ajax('https://secure.bixi.com/data/stations.json',
+        {
+            success: function(data){
+                var bixi = data
+                var mark = new google.maps.Marker({
+                    position: {lat: data.stations[0].la, lng: data.stations[0].lo},
+                    map: map
+                })
+            },
+            error: function(jqXHR, status, error){
+                alert(error);
+            }
+        }
+    );
 }
